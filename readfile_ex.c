d10466b8318863290bcec4e9b5bc92fc99806627
fxbrit <fxbrit@tuta.io>
2021-11-02 14:39:50 +0100

    print traffic on port 80

--- readfile_ex.c
+++ readfile_ex.c
@@ -1,10 +1,10 @@
-// include I/O functions
+// Standard C include file for I/O functions
 #include <stdio.h>
 
-// include libpcap
+// Include files for libpcap functions
 #include <pcap.h>
 
-// include struct of ethernet, ip and tcp header
+// Includes struct of ethernet, ip and tcp header
 #include <netinet/if_ether.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
@@ -19,16 +19,18 @@ int main(int argc, char **argv)
 	const u_char *pkt_data;
 	u_int i=0;
 	int res;
-	char *dev = "enp1s0"; // device name
+	char *dev = "enp1s0"; // device
 	struct ether_header *eptr; // struct of ethernet header
 
 	/* live sniffing from device.
-	 * params are:
-	 * device - device to capture from, in our case enp1s0
-     * snaplen - max size of packets to capture
-     * promisc - set promiscous mode, 0 is off
-     * to_ms - time to wait in ms before timeout, do not use -1
-     * errbuf  - error buffer
+
+       pcap_t *pcap_open_live(char *device,int snaplen, int prmisc,int to_ms,
+       char *ebuf)
+
+       snaplen - max size of packets to capture
+       promisc - set promiscous mode or not, 0 is off
+       to_ms   - time to wait in ms before timeout, do not use -1
+       errbuf  - error buffer
 	 */
 	if ((fp = pcap_open_live(dev, BUFSIZ, 0, 10, errbuf)) == NULL)
 	{
@@ -36,21 +38,11 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	
-	/* retrieve the packets */
+	/* Retrieve the packets */
 	while((res = pcap_next_ex(fp, &header, &pkt_data)) >= 0)
 	{	
-		printf("%u\n", header->caplen);
-		/* print timestamp and len */
-		printf("%ld:%ld (%d)\n", header->ts.tv_sec, header->ts.tv_usec, header->len);
-		
-		// cast packet into ethernet header struct
+		/* cast packet into ethernet header struct */
 		eptr = (struct ether_header *) pkt_data;
-		// print source and dest mac
-		printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x --> %02x:%02x:%02x:%02x:%02x:%02x\n",
-				eptr->ether_shost[0], eptr->ether_shost[1], eptr->ether_shost[2],
-				eptr->ether_shost[3], eptr->ether_shost[4], eptr->ether_shost[5],
-				eptr->ether_dhost[0], eptr->ether_dhost[1], eptr->ether_dhost[2],
-				eptr->ether_dhost[3], eptr->ether_dhost[4], eptr->ether_dhost[5]);
 
 		if (ntohs (eptr->ether_type) == ETHERTYPE_IP) {
 			struct iphdr *ipptr; // struct of ip header
@@ -71,23 +63,37 @@ int main(int argc, char **argv)
 
 			struct tcphdr *tcphead;
 
-			tcphead = (struct tcphdr *) pkt_data + 14 + ip_hl; // jump past the ip header
-			int source_port = ntohs(tcphead->th_sport); // source tcp port
-			int dest_port = ntohs(tcphead->th_dport); // dest tcp port
+			tcphead = (struct tcphdr *) &pkt_data[14 + ip_hl]; // jump past the ip header
+			int source_port = ntohs(tcphead->th_sport);
+			int dest_port = ntohs(tcphead->th_dport);
+			int tcp_off = ((int)tcphead->th_off)*4;
+			char *payload = (char *) &pkt_data[14 + ip_hl + tcp_off];
 
-			/* print IP addresses of each packet, by using bit masking */
-			printf("IP: %i.%i.%i.%i --> %i.%i.%i.%i\nIP packet length: %i byte\nProtocol %s on port %i --> %i\n\n",
-			(source_ip & 0xff000000) >> 24, (source_ip & 0x00ff0000) >> 16, (source_ip & 0x0000ff00) >> 8, source_ip & 0x000000ff,
-			(dest_ip & 0xff000000) >> 24, (dest_ip & 0x00ff0000) >> 16, (dest_ip & 0x0000ff00) >> 8, dest_ip & 0x000000ff,
-			ntohs(ipptr->tot_len),
-			t_proto,
-			source_port,
-			dest_port
-			);
+			if (dest_port == 80) { // check if traffic is http
+				/* print timestamp and len 
+				printf("%ld:%ld ", header->ts.tv_sec, header->ts.tv_usec);
+
+				/* print source and dest mac */
+				printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x --> %02x:%02x:%02x:%02x:%02x:%02x ",
+				eptr->ether_shost[0], eptr->ether_shost[1], eptr->ether_shost[2],
+				eptr->ether_shost[3], eptr->ether_shost[4], eptr->ether_shost[5],
+				eptr->ether_dhost[0], eptr->ether_dhost[1], eptr->ether_dhost[2],
+				eptr->ether_dhost[3], eptr->ether_dhost[4], eptr->ether_dhost[5]);
+
+				/* Print on screen the IP addresses of each packet, by using bit masking */
+				printf("%i.%i.%i.%i --> %i.%i.%i.%i %s %i --> %i\n",
+				(source_ip & 0xff000000) >> 24, (source_ip & 0x00ff0000) >> 16, (source_ip & 0x0000ff00) >> 8, source_ip & 0x000000ff,
+				(dest_ip & 0xff000000) >> 24, (dest_ip & 0x00ff0000) >> 16, (dest_ip & 0x0000ff00) >> 8, dest_ip & 0x000000ff,
+				t_proto,
+				source_port,
+				dest_port
+				);
+
+				printf("%s", payload); // wrong, it should print payload and not anything past it
+				printf("\n\n");
+			}
 
  		}
-		
-		printf("\n\n");
 	}
 	
 	if (res == -1)
